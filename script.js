944c6989e96b7ded4ef13c266e44e921b36dfe22
Bassirou Diop <subad928@gmail.com>
2025-05-15 19:38:32 -0500

    If else

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
-const x = 6
-const y = "7"
+if (true) {
+    console.log('hello world')
+} else if (false) {
+    console.log("")
+} else if () {
 
-console.log(y + x)
+} else {
+    
+}
+
+// console.log("1" == 1)
+
+// console.log("1" != 2)
+
+// console.log("1" !== 1)
+
+// console.log(1 === 1)
+
+// const x = 2345
+// const y = 2
+
+// console.log(String(x) + y)
+
+// const x = 2345
+// const y = 2
+
+// console.log(x + "")
+
+// const x = 2345
+// const y = 2
+
+// console.log(x.toString())
+
+// const x = "2345.8"
+// const y = 2
+
+// console.log(Number(true) + y)
+
+// const x = "2345.8"
+// const y = 2
+
+// console.log(Number(x) + y)
+
+// const x = "2345px"
+// const y = 2
+
+// console.log(parseInt(x) + y)
+
+// const x = "2345.4px"
+// const y = 2
+
+// console.log(parseFloat(x) + y)
+
+// const x = "2345"
+// const y = 2
+
+// console.log(x * y)
+
+// const x = 6
+// const y = "7"
+
+// console.log(y + x)
 
 // function test() {
 //         console.log(x)
